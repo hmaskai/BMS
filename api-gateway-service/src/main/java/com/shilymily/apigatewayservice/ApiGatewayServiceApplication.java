c25fa44cfd7c9f9eafb9c221fbaf55b34b658c87
Sneha Bharadwaj <snehbhar@amazon.com>
2020-05-24 21:40:34 -0700

    [REGISTRY SERVER] Registry server using eureka - initial framework

--- api-gateway-service/src/main/java/com/shilymily/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/com/shilymily/apigatewayservice/ApiGatewayServiceApplication.java
@@ -2,7 +2,9 @@ package com.shilymily.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayServiceApplication {
 
